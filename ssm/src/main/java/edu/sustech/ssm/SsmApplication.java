b20f2b000886877ed7dae970799f7f9d0e29455f
gfzum <gfzmder@gmail.com>
2022-05-22 21:19:34 +0800

    files

---
+++ ssm/src/main/java/edu/sustech/ssm/SsmApplication.java
@@ -0,0 +1,13 @@
+package edu.sustech.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmApplication.class, args);
+    }
+
+}
