b0e44f69c4f40f1d0f86415c4bc5121931eef307
QAQGaeBolg <11911109@mail.sustech.edu.cn>
2022-05-22 19:59:31 +0800

    update

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-    <!--img alt="Vue logo" src="./assets/logo.png"-->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>  
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
@@ -21,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
